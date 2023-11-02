b05571c6fa401627d69611ee5396150134f1a84d
Kimhyunjin4455 <dp4455@naver.com>
2023-11-02 16:42:06 +0900

    swea1970

--- d2/1970.py
+++ d2/1970.py
@@ -2,9 +2,13 @@ T = int(input())
 for test_case in range(1, T+1):
     n = int(input())
     coin_list = [50000,10000,5000,1000,500,100,50,10]
-    change = [0,0,0,0,0,0,0,0]
-    for coin in range(len(coin_list)):
-        if n > coin_list[coin]:
-            n -= (coin * (n // coin))
-            change[coin] += 1
-    print("")
+    change_list = [0,0,0,0,0,0,0,0]
+    result = ""
+    for i in range(len(coin_list)):
+        if n >= coin_list[i]:
+            change_list[i] += (n // coin_list[i] )
+            n -= (coin_list[i] * (n // coin_list[i]))
+    for change in change_list:
+        result += (str(change)+" ")
+    print("#{}".format(test_case))
+    print(result)
