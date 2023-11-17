aa27a9f7d35e1c2fa08c9e5d7c0ebabce01402e6
Kimhyunjin4455 <dp4455@naver.com>
2023-11-17 17:18:56 +0900

    swea1954

--- d2/1954.py
+++ d2/1954.py
@@ -1,1 +1,50 @@
-##
+T = int(input())
+for test_case in range(1, T+1):
+    n = int(input()) # 크기
+    area = [[0] * n for _ in range(n)] # 달팽이 모양
+
+    dr = [0,1,0,-1]
+    dc = [1,0,-1,0]
+
+    now_dir = 0
+    row = 0
+    column = 0
+
+    for i in range(1, (n*n)+1):
+        area[row][column] = i
+        row += dr[now_dir]
+        column += dc[now_dir]
+
+        if row < 0 or column <0 or row >= n or column >=n or area[row][column] != 0: # 최대 길이거나 값이 입력되어 있는 상태면
+            row -= dr[now_dir]
+            column -= dc[now_dir]
+
+            now_dir = (now_dir+1) % 4 # 4부터는 dr/dc의 범위를 초과하기에 %4
+            row += dr[now_dir]
+            column += dc[now_dir]
+
+    print("#{}".format(test_case))
+    for ary in area:
+        print(*ary)
+
+
+
+
+
+
+
+    # 열이 끝까지 커지고(값이 0이 아닐때까지), 값 넣고 num+1
+    # 행이 끝까지 커지고(..), ..
+    # 열이 끝까지 작아지고(..), ..
+    # 행이 끝까지 작아지고(
+
+
+
+
+
+
+
+
+
+
+
