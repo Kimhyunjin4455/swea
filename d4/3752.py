c614100be10c1f3b9fa950b6aeb28eff66525e48
Kimhyunjin4455 <dp4455@naver.com>
2023-11-30 15:50:36 +0900

    swea2819

--- d4/3752.py
+++ d4/3752.py
@@ -1,30 +1,19 @@
 T = int(input())
-global result
-
-def scores_cnt(arr):
-    arr.insert(0,0)
-    arr_sub = arr.copy()
-    result = [0]
-    for i in range(N+1):
-        now = arr[i]
-        for j in range(N+1): # 02 23 03 55 25 35
-            if i != j:
-                now += arr_sub[j]
-                if now not in result: # 기존의 값에 존재하지 않아야 추가
-                    result.append(now)
-                if (arr[i] + arr_sub[j]) not in result:
-                    result.append(arr[i] + arr_sub[j])
-    return result
-
-
-
-for tc in range(1,T+1):
+for tc in range(1, T+1):
     N = int(input())
-    scores = list(map(int, input().split()))
-    final = scores_cnt(scores)
-    result = []
+    arr = list(map(int, input().split()))
 
-    print("#{} {}".format(tc,len(final)))
+    visited = [1] + [0] * sum(arr)
+    nums = [0]
+
+    for i in arr:
+        for j in range(len(nums)):
+            # i==2 -> nums {0,2}
+            # i==3 -> nums {0,2,3,5}
+            # i==5 -> nums {0,2,3,5,7,8,10}
+            if visited[i + nums[j]] == 0:
+                visited[i+nums[j]] = 1
+                nums.append(i+nums[j])
 
 
-
+    print("#{} {}".format(tc, len(nums)))
